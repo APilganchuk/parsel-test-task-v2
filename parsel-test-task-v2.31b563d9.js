81a0c228e4d71d80e92f30a5010304547b47a366
APilganchuk <opilganchuk@gmail.com>
2026-03-10 12:45:34 +0200

    Updates

--- parsel-test-task-v2.31b563d9.js
+++ parsel-test-task-v2.31b563d9.js
@@ -12011,11 +12011,12 @@ const API_URL = 'https://veryfast.io/t/front_test_api.php';
 async function fetchLicenses() {
     try {
         const res = await fetch(API_URL);
-        if (!res.ok) throw new Error(res.statusText);
+        if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
         const data = await res.json();
         return data?.result?.elements || [];
     } catch (err) {
         console.error('Error fetching licenses:', err);
+        return [];
     }
 }
 
